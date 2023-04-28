4112831e2f2e9713a4931b0569aa830bf3dab092
VadymMurzin <prometey736@gmail.com>
2023-04-28 22:36:30 +0200

    Add image gallery and modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  const close = {
+    onShow: () => document.addEventListener("keydown", keyCloseModal),
+    onClose: () => document.removeEventListener("keydown", keyCloseModal),
+  };
+
+  instance.show();
+  close.onShow();
+
+  function keyCloseModal(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+
+  instance
+    .element()
+    .querySelector(".basicLightbox__placeholder")
+    .addEventListener("click", () => {
+      instance.close();
+      close.onClose();
+    });
+}
+
+// console.log(galleryItems);
